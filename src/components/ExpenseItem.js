9e37be7036b86dd74d24f115f3edcd5e3c9d6c16
Suchitra <suchitra.basavaraju@thoughtworks.com>
2022-09-08 12:53:53 +0530

    Add. Extract expense date as a component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,11 @@
 import React from "react";
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 export default function ExpenseItem(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
-
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
-      <div> {month}</div>
-      <div> {day}</div>
-      <div> {year}</div>
+      <ExpenseDate date={props.date} />
 
       <div className="expense-item-title">
         <h2>{props.title} </h2>
